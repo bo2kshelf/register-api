290638b9dce930c185c5a273de39dbaf2e8a3fc0
SnO2WMaN <sno2wman.official@gmail.com>
2020-12-06 21:49:57 +0900

    feat: :sparkles: MongoDBへの接続をURIだけで完結

--- src/mongoose/mongoose.service.ts
+++ src/mongoose/mongoose.service.ts
@@ -1,7 +1,6 @@
 import {Inject, Injectable} from '@nestjs/common';
 import {ConfigType} from '@nestjs/config';
 import {MongooseModuleOptions, MongooseOptionsFactory} from '@nestjs/mongoose';
-import {format as formatMongoURI} from 'mongodb-uri';
 import mongooseConfig from './mongoose.config';
 
 @Injectable()
@@ -13,14 +12,7 @@ export class MongooseService implements MongooseOptionsFactory {
 
   createMongooseOptions(): MongooseModuleOptions {
     return {
-      uri: formatMongoURI({
-        scheme: 'mongodb',
-        options: {authSource: 'admin'},
-        hosts: this.configService.hosts,
-        database: this.configService.database,
-        username: this.configService.username,
-        password: this.configService.password,
-      }),
+      uri: this.configService.uri,
     };
   }
 }
