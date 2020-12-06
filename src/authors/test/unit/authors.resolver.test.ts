15e7c4966d1ab56cf830c3220c9a6890855e2e6f
SnO2WMaN <sno2wman.official@gmail.com>
2020-12-06 22:49:17 +0900

    feat: :sparkles: Author関連実装

---
+++ src/authors/test/unit/authors.resolver.test.ts
@@ -0,0 +1,115 @@
+import {getModelToken, MongooseModule} from '@nestjs/mongoose';
+import {Test, TestingModule} from '@nestjs/testing';
+import {MongoMemoryServer} from 'mongodb-memory-server';
+import {Model} from 'mongoose';
+import {AuthorsResolver} from '../../authors.resolver';
+import {AuthorsService} from '../../authors.service';
+import {Author, AuthorSchema} from '../../schema/author.schema';
+
+describe('AuthorResolver', () => {
+  let mongoServer: MongoMemoryServer;
+
+  let module: TestingModule;
+
+  let authorModel: Model<Author>;
+
+  let authorService: AuthorsService;
+  let authorResolver: AuthorsResolver;
+
+  beforeAll(async () => {
+    mongoServer = new MongoMemoryServer();
+  });
+
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRootAsync({
+          useFactory: async () => ({uri: await mongoServer.getUri()}),
+        }),
+        MongooseModule.forFeature([{name: Author.name, schema: AuthorSchema}]),
+      ],
+      providers: [
+        {
+          provide: AuthorsService,
+          useValue: {
+            getById() {},
+            id: (Author: Author) => Author._id,
+            create() {},
+          },
+        },
+        AuthorsResolver,
+      ],
+    }).compile();
+
+    authorModel = module.get<Model<Author>>(getModelToken(Author.name));
+
+    authorService = module.get<AuthorsService>(AuthorsService);
+    authorResolver = module.get<AuthorsResolver>(AuthorsResolver);
+  });
+
+  afterEach(async () => {
+    jest.clearAllMocks();
+
+    await authorModel.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await mongoServer.stop();
+
+    await module.close();
+  });
+
+  it('should be defined', () => {
+    expect(authorResolver).toBeDefined();
+  });
+
+  describe('Author()', () => {
+    it('存在するならばそれを返す', async () => {
+      const newAuthor = await authorModel.create({name: 'コトヤマ'});
+
+      jest.spyOn(authorService, 'getById').mockResolvedValueOnce(newAuthor);
+
+      const actual = await authorResolver.author(newAuthor._id);
+
+      expect(actual).toHaveProperty('name', 'コトヤマ');
+    });
+
+    it('存在しない場合はErrorを返す', async () => {
+      jest
+        .spyOn(authorService, 'getById')
+        .mockRejectedValueOnce(
+          new Error(
+            `Author associated with ID "5fccac3585e5265603349e97" doesn't exist.`,
+          ),
+        );
+
+      await expect(() =>
+        authorResolver.author('5fccac3585e5265603349e97'),
+      ).rejects.toThrow(
+        `Author associated with ID "5fccac3585e5265603349e97" doesn't exist.`,
+      );
+    });
+  });
+
+  describe('id()', () => {
+    it('適切なIDを返す', async () => {
+      const newAuthor = await authorModel.create({name: 'コトヤマ'});
+
+      const actual = await authorResolver.id(newAuthor);
+
+      expect(actual).toBe(newAuthor._id);
+    });
+  });
+
+  describe('createAuthor()', () => {
+    it('全てのプロパティが存在する', async () => {
+      const newAuthor = await authorModel.create({name: 'コトヤマ'});
+
+      jest.spyOn(authorService, 'create').mockResolvedValueOnce(newAuthor);
+
+      const actual = await authorResolver.createAuthor({name: 'コトヤマ'});
+
+      expect(actual).toHaveProperty('name', 'コトヤマ');
+    });
+  });
+});
