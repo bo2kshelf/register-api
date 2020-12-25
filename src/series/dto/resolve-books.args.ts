d75a9206bbdd96ecf95611d55b668745d93440e7
SnO2WMaN <sno2wman.official@gmail.com>
2020-12-26 04:17:22 +0900

    refactor: SeriesResolveBooksOrderの名前ミス修正

--- src/series/dto/resolve-books.args.ts
+++ src/series/dto/resolve-books.args.ts
@@ -2,14 +2,14 @@ import {ArgsType, Field, InputType} from '@nestjs/graphql';
 import {RequiredPaginationArgs} from '../../paginate/dto/required-pagination.args';
 import {OrderDirection} from '../../paginate/enum/order-direction.enum';
 
-@InputType('SerialResolveBooksOrder')
-export class SerialResolveBooksOrderInput {
+@InputType('SeriesResolveBooksOrder')
+export class SeriesResolveBooksOrderInput {
   @Field((_type) => OrderDirection, {nullable: true})
   serial?: OrderDirection;
 }
 
 @ArgsType()
 export class SeriesResolveBooksArgs extends RequiredPaginationArgs {
-  @Field((_type) => SerialResolveBooksOrderInput, {nullable: true})
-  orderBy?: SerialResolveBooksOrderInput;
+  @Field((_type) => SeriesResolveBooksOrderInput, {nullable: true})
+  orderBy?: SeriesResolveBooksOrderInput;
 }
