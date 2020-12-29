2d2103d2f2d2811c5b2a8989a9d9619c14344960
SnO2WMaN <sno2wman.official@gmail.com>
2020-12-30 03:54:34 +0900

    test: Authorのsmallテスト書き直し

--- src/authors/test/small/author.service.test.ts
+++ src/authors/test/small/author.service.test.ts
@@ -12,30 +12,33 @@ describe(AuthorsService.name, () => {
 
   let authorModel: Model<Author>;
 
-  let authorService: AuthorsService;
   let paginateService: PaginateService;
+
+  let authorService: AuthorsService;
 
   beforeEach(async () => {
     module = await Test.createTestingModule({
       providers: [
         {
           provide: getModelToken(Author.name),
-          useValue: {id() {}, async findById() {}, async create() {}},
+          useValue: {
+            find() {},
+            findById() {},
+            create() {},
+          },
         },
         {
           provide: PaginateService,
-          useValue: {
-            getConnectionFromMongooseModel() {},
-          },
+          useValue: {getConnectionFromMongooseModel() {}},
         },
         AuthorsService,
       ],
     }).compile();
 
     authorModel = module.get<Model<Author>>(getModelToken(Author.name));
+    paginateService = module.get<PaginateService>(PaginateService);
 
     authorService = module.get<AuthorsService>(AuthorsService);
-    paginateService = module.get<PaginateService>(PaginateService);
   });
 
   afterEach(async () => {
@@ -51,57 +54,62 @@ describe(AuthorsService.name, () => {
   });
 
   describe('getById()', () => {
-    it('idから見つかる場合はそのまま返す', async () => {
+    it('正常に取得できたらそれを返す', async () => {
       jest.spyOn(authorModel, 'findById').mockResolvedValueOnce({} as Author);
+
       const actual = await authorService.getById(new ObjectId());
       expect(actual).toBeDefined();
     });
 
-    it('idから見つからない場合は例外を投げる', async () => {
+    it('存在しない場合は例外を投げる', async () => {
       jest.spyOn(authorModel, 'findById').mockResolvedValueOnce(null);
+
       await expect(() => authorService.getById(new ObjectId())).rejects.toThrow(
         NoDocumentForObjectIdError,
       );
     });
   });
 
-  describe('id()', () => {
-    it('Documentの_idを返す', async () => {
-      const expected = new ObjectId();
-      const book: Author = {_id: expected} as Author;
+  describe('all()', () => {
+    it('受け取ったものをそのまま返す', async () => {
+      jest.spyOn(authorModel, 'find').mockResolvedValueOnce([{} as Author]);
 
-      const actual = authorService.id(book);
+      const actual = await authorService.all();
+      expect(actual).toBeDefined();
+    });
+  });
+
+  describe('id()', () => {
+    it('引数の_idをそのまま返す', () => {
+      const expected = new ObjectId();
+
+      const actual = authorService.id({_id: expected} as Author);
 
       expect(actual).toStrictEqual(expected);
     });
   });
 
   describe('create()', () => {
-    it('期待されるプロパティが全て存在する', async () => {
+    it('受け取ったものをそのまま返す', async () => {
       jest.spyOn(authorModel, 'create').mockResolvedValueOnce({} as Author);
 
-      const actual = authorService.create({
-        name: 'name',
-      });
-
+      const actual = await authorService.create({name: 'Name'});
       expect(actual).toBeDefined();
     });
   });
 
   describe('books()', () => {
-    it('正常な動作', async () => {
+    it('受け取ったものをそのまま返す', async () => {
+      jest.spyOn(authorService, 'id').mockReturnValue(new ObjectId());
       jest
         .spyOn(paginateService, 'getConnectionFromMongooseModel')
-        .mockResolvedValue({
+        .mockResolvedValueOnce({
           aggregate: {count: 0},
-          edges: [],
           pageInfo: {},
+          edges: [],
         });
 
-      const actual = authorService.books({_id: new ObjectId()} as Author, {
-        first: 1,
-      });
-
+      const actual = await authorService.books({} as Author, {});
       expect(actual).toBeDefined();
     });
   });
