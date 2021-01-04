e4ee08eabf9d327ea32b113cbba07cc50f8ca5f3
SnO₂WMaN <sno2wman.official@gmail.com>
2021-01-05 06:54:08 +0900

    fix: SeriesRelatedBooksConnectionResolverを修正 (#95)

--- src/books/connection/series-connection.resolver.ts
+++ src/books/connection/series-connection.resolver.ts
@@ -26,4 +26,14 @@ export class SeriesBooksConnectionResolver {
   /* istanbul ignore next */
   () => SeriesRelatedBooksConnection,
 )
-export class SeriesRelatedBooksConnectionResolver extends SeriesBooksConnectionResolver {}
+export class SeriesRelatedBooksConnectionResolver {
+  constructor(private bookService: BooksService) {}
+
+  @ResolveField(
+    /* istanbul ignore next */
+    () => BookEntity,
+  )
+  async book(@Parent() {id}: SeriesBooksConnection) {
+    return this.bookService.getById(id);
+  }
+}
